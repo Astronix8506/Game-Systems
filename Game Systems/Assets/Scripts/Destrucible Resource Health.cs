c410b69313ebf357affa98398f54bcc5c1d337a7
Astronix8506 <113216692+Astronix8506@users.noreply.github.com>
2026-02-02 13:24:03 -0500

    Resource Stages
    Added a stage system to the resource that can be customized.
    Auto checks if stage precents were implemented correctly.
    Changes sprite according to implemented user stage.

--- Game Systems/Assets/Scripts/Destrucible Resource Health.cs
+++ Game Systems/Assets/Scripts/Destrucible Resource Health.cs
@@ -1,26 +1,70 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class DestrucibleResourceHealth : MonoBehaviour
 {
-    [SerializeField] private int maxHealth;
-    [SerializeField] private int currentHealth;
+    [SerializeField] private float maxHealth;
+    [SerializeField] private float currentHealth;
+    [SerializeField]private List<ResourceStage> _resourceStages = new();
     
+    private int currentStage = 0;
+    public SpriteRenderer spriteRenderer;
     void Start()
     {
+        PercentCheck();
         currentHealth = maxHealth;
+        spriteRenderer.sprite = _resourceStages[currentStage].Stageicon;
     }
 
     public void TakeDamagePercent(int percentDamage)
     {
         float percent = percentDamage / 100f;
-        int damage = Mathf.RoundToInt(maxHealth * percent);
+        float damage = maxHealth * percent;
         currentHealth -= damage;
-        CheckHealth();
+        Debug.Log(gameObject.name+ " Took damage: " + damage);
+        StageCheck();
     }
-    private void CheckHealth()
+
+    //Debug purposes
+    public void PercentCheck()
     {
-        if (currentHealth <= 0)
+        int listCount = _resourceStages.Count;
+        if (listCount == 0) return;
+        
+        if (_resourceStages[0].requiredPercent != 100) 
+            Debug.LogWarning(gameObject.name + " Element ["+ (listCount - 1) +"] Incorrect percent - MUST BE 100");
+        
+        if (_resourceStages[listCount - 1].requiredPercent != 0) 
+            Debug.LogWarning(gameObject.name + " Element [0] Incorrect Percent - MUST BE 0");
+        
+        for (int i = 0; i < listCount - 2; i++)
+        {
+            if (_resourceStages[i].requiredPercent < _resourceStages[i + 1].requiredPercent)
+                Debug.LogWarning(gameObject.name + " Element ["+ (i+1) +"] not in correct descending order");
+        }
+    }
+    
+    public void StageCheck()
+    {
+        float healthPercent = (currentHealth / maxHealth) * 100;
+        int nextStagePercent;
+        ;
+        nextStagePercent = _resourceStages[currentStage + 1].requiredPercent;
+        
+        Debug.Log("Amount of Stages: "+_resourceStages.Count+"  On Stage: "+currentStage+"  Next Stage : "+nextStagePercent +
+                  "  Current Percent: " +healthPercent + "Current health: " + currentHealth + "  Maxhealth: " + maxHealth);
+        
+        if (healthPercent <= nextStagePercent)
+            currentStage++;
+        
+        if (_resourceStages[currentStage].Stageicon)
+            spriteRenderer.sprite = _resourceStages[currentStage].Stageicon;
+
+        if (currentStage == _resourceStages.Count - 1)
+        {
+            //temporary destroy
             Destroy(this.gameObject);
+        }
     }
 }
--- Game Systems/Assets/Scripts/TestScript.cs
+++ Game Systems/Assets/Scripts/TestScript.cs
@@ -2,26 +2,24 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
-    public GameObject targetObject;
+    private string targetObject = "Resource";
     private DestrucibleResourceHealth destructableHealth;
     [SerializeField] private int percentDamage;
+    
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Vector3 mouseScreenPos = Input.mousePosition;
-            Vector3  mouseWorldPos = Camera.main.ScreenToWorldPoint(mouseScreenPos);
-            Vector2 rayOrgin = new Vector2(mouseWorldPos.x, mouseWorldPos.y);
+        if (!Input.GetMouseButtonDown(0)) return;
+        Vector3 mouseScreenPos = Input.mousePosition;
+        Vector3  mouseWorldPos = Camera.main.ScreenToWorldPoint(mouseScreenPos);
+        Vector2 rayOrgin = new Vector2(mouseWorldPos.x, mouseWorldPos.y);
             
-            Collider2D hit = Physics2D.OverlapPoint(rayOrgin);
-            if (hit.CompareTag(targetObject.tag))
-            {
-                destructableHealth = hit.GetComponent<DestrucibleResourceHealth>();
-                destructableHealth.TakeDamagePercent(percentDamage);
-            }
-        }
+        Collider2D hit = Physics2D.OverlapPoint(rayOrgin);
         
+        if (!hit || !hit.CompareTag(targetObject)) return;
+        destructableHealth = hit.GetComponent<DestrucibleResourceHealth>();
+        destructableHealth.TakeDamagePercent(percentDamage);
+
     }
     
     
